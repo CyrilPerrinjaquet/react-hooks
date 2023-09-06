0ee09c4409501f36acfad88851e534aedbd0cfae
CyrilPerrinjaquet <cyrilp.perso@gmail.com>
2023-09-06 13:33:12 +0200

    :sparkles: Completed exercise 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,10 +5,11 @@ import * as React from 'react'
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState('')
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
@@ -23,7 +24,29 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Kent" />
 }
 
 export default App
+
+/* Extra Credit
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+*/
